a1a97c826645caeebbb9c0f3462c4b0841629513
Arthur Melin <arthur.melin@epitech.eu>
2018-01-05 13:10:39 +0100

    LOL

--- ex01/String.c
+++ ex01/String.c
@@ -7,16 +7,58 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include "String.h"
+#include "./String.h"
 
 static void assign_s(String *, const String *);
 static void assign_c(String *, const char *);
+static void append_s(String *, const String *);
+static void append_c(String *, const char *);
+static char at(String *, size_t);
+static void clear(String *);
+static int size(String *);
+static int compare_s(String *, const String *);
+static int compare_c(String *, const char *);
+static size_t copy(String *, char *, size_t, size_t);
+static const char *c_str(String *);
+static int empty(String *);
+static int find_s(String *, const String *, size_t);
+static int find_c(String *, const char *, size_t);
+static void insert_s(String *, size_t, const String *);
+static void insert_c(String *, size_t, const char *);
+static int to_int(String *);
+static String *split_s(String *, char);
+static char **split_c(String *, char);
+static void aff(String *);
+static void join_s(String *, char, String *);
+static void join_c(String *, char, const char **);
+static String *substr(String *, int, int);
 
 void StringInit(String *this, const char *s)
 {
 	this->str = strdup(s);
 	this->assign_s = assign_s;
 	this->assign_c = assign_c;
+	this->append_s = append_s;
+	this->append_c = append_c;
+	this->at = at;
+	this->clear = clear;
+	this->size = size;
+	this->compare_s = compare_s;
+	this->compare_c = compare_c;
+	this->copy = copy;
+	this->c_str = c_str;
+	this->empty = empty;
+	this->find_s = find_s;
+	this->find_c = find_c;
+	this->insert_s = insert_s;
+	this->insert_c = insert_c;
+	this->to_int = to_int;
+	this->split_s = split_s;
+	this->split_c = split_c;
+	this->aff = aff;
+	this->join_s = join_s;
+	this->join_c = join_c;
+	this->substr = substr;
 }
 
 void StringDestroy(String *this)
@@ -27,11 +69,106 @@ void StringDestroy(String *this)
 
 static void assign_s(String *this, const String *str)
 {
-	this->assign_c(str->str);
+	this->assign_c(this, str->str);
 }
 
 static void assign_c(String *this, const char *s)
 {
 	free(this->str);
-	this->str = s;
+	this->str = strdup(s);
+}
+
+static void append_s(String *this, const String *ap)
+{
+	this->append_c(this, ap->str);
+}
+
+static void append_c(String *this, const char *ap)
+{
+	char *res;
+
+	res = malloc(strlen(this->str) + strlen(ap) + 1);
+	strcpy(res, this->str);
+	strcat(res, ap);
+	this->assign_c(this, res);
+}
+
+static char at(String *this, size_t pos)
+{
+	if (pos >= strlen(this->str))
+		return (-1);
+	return (this->str[pos]);
+}
+
+static void clear(String *this)
+{
+	this->assign_c(this, "");
+}
+
+static int size(String *this)
+{
+}
+
+static int compare_s(String *this, const String *str)
+{
+}
+
+static int compare_c(String *this, const char *str)
+{
+}
+
+static size_t copy(String *this, char *s, size_t n, size_t pos)
+{
+}
+
+static const char *c_str(String *this)
+{
+}
+
+static int empty(String *this)
+{
+}
+
+static int find_s(String *this, const String *str, size_t pos)
+{
+}
+
+static int find_c(String *this, const char *str, size_t pos)
+{
+}
+
+static void insert_s(String *this, size_t pos, const String *str)
+{
+}
+
+static void insert_c(String *this, size_t pos, const char *str)
+{
+}
+
+static int to_int(String *this)
+{
+}
+
+static String *split_s(String *this, char separator)
+{
+}
+
+static char **split_c(String *this, char separator)
+{
+}
+
+static void aff(String *this)
+{
+}
+
+static void join_s(String *this, char delim, String *tab)
+{
+}
+
+static void join_c(String *this, char delim, const char **tab)
+{
+}
+
+static String *substr(String *this, int offset, int length)
+{
 }
--- ex01/String.h
+++ ex01/String.h
@@ -8,10 +8,33 @@
 #if !defined (STRING_H_)
 	#define STRING_H_
 
+	#include <stdlib.h>
+
 typedef struct string {
 	char *str;
-	void (*assign_s)(String *, const String *);
-	void (*assign_c)(String *, const char *);
+	void (*assign_s)(struct string *, const struct string *);
+	void (*assign_c)(struct string *, const char *);
+	void (*append_s)(struct string *, const struct string *);
+	void (*append_c)(struct string *, const char *);
+	char (*at)(struct string *, size_t);
+	void (*clear)(struct string *);
+	int (*size)(struct string *);
+	int (*compare_s)(struct string *, const struct string *);
+	int (*compare_c)(struct string *, const char *);
+	size_t (*copy)(struct string *, char *, size_t, size_t);
+	const char *(*c_str)(struct string *);
+	int (*empty)(struct string *);
+	int (*find_s)(struct string *, const struct string *, size_t);
+	int (*find_c)(struct string *, const char *, size_t);
+	void (*insert_s)(struct string *, size_t, const struct string *);
+	void (*insert_c)(struct string *, size_t, const char *);
+	int (*to_int)(struct string *);
+	struct string *(*split_s)(struct string *, char);
+	char **(*split_c)(struct string *, char);
+	void (*aff)(struct string *);
+	void (*join_s)(struct string *, char, struct string *);
+	void (*join_c)(struct string *, char, const char **);
+	struct string *(*substr)(struct string *, int, int);
 } String;
 
 void StringInit(String *, const char *);
